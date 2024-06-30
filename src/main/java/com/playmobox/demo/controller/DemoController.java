4fe74f854aedfc806570697aecb5d1795f39e0b6
Sufian Abu-Rab <s.aburab@gmail.com>
2024-06-30 14:26:31 +0200

    first commit

---
+++ src/main/java/com/playmobox/demo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.playmobox.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String Home() {
+        return "home";
+    }
+}
